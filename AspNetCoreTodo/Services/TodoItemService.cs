71284a2f9eb19d26afee8e8653bea197cab5aa27
Robert Lewis <rlewis@relias.com>
2019-08-01 11:23:05 -0400

    adding entity framework and database migrations

---
+++ AspNetCoreTodo/Services/TodoItemService.cs
@@ -0,0 +1,29 @@
+﻿using AspNetCoreTodo.Data;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AspNetCoreTodo.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TodoItem[]> GetIncompleteItemsAsync()
+        {
+            var items = await _context.Items
+                .Where(x => x.IsDone == false)
+                .ToArrayAsync();
+            return items;
+        }
+    }
+}
