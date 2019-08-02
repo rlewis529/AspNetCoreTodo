1699032f0adadfbf3f3f15afb81c9752fc5f6bbd
Robert Lewis <rlewis@relias.com>
2019-08-02 15:01:23 -0400

    adding admin seeding etc

---
+++ AspNetCoreTodo/Controllers/ManageUsersController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using AspNetCoreTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using AspNetCoreTodo.Services;
+using AspNetCoreTodo;
+
+namespace AspNetCoreTodo.Controllers
+{
+    [Authorize(Roles = Constants.AdministratorRole)]
+    public class ManageUsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser>
+               _userManager;
+
+        public ManageUsersController(
+            UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var admins = (await _userManager
+                .GetUsersInRoleAsync(Constants.AdministratorRole))
+                .ToArray();
+
+            var everyone = await _userManager.Users
+                .ToArrayAsync();
+
+            var model = new ManageUsersViewModel
+            {
+                Administrators = admins,
+                Everyone = everyone
+            };
+
+            return View(model);
+        }
+
+
+
+    }
+}
---
+++ AspNetCoreTodo/Models/ManageUsersViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Services;
+
+namespace AspNetCoreTodo.Models
+{
+    public class ManageUsersViewModel
+    {
+        public ApplicationUser[] Administrators { get; set; }
+
+        public ApplicationUser[] Everyone { get; set; }
+    }
+}
